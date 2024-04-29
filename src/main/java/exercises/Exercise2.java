6c036aeb909dd0073de3eb9c468e177dba90627b
ste <mrbarrow@hotmail.co.uk>
2024-04-29 12:19:27 +0100

    fix: ex 1 scanner handling

--- src/main/java/exercises/Exercise2.java
+++ src/main/java/exercises/Exercise2.java
@@ -7,17 +7,17 @@ public class Exercise2 {
 
     public static int[] getUserInput() {
 
-        Scanner scanner;
+        Scanner scanner = new Scanner(System.in);
 
         while (true) {
             try {
-                scanner = new Scanner(System.in);
                 System.out.print("Enter divisor: ");
                 int divisor = scanner.nextInt();
                 System.out.print("Enter dividend: ");
                 int dividend = scanner.nextInt();
                 return new int[]{divisor, dividend};
             } catch (InputMismatchException e) {
+                scanner.next();
                 System.out.println("Invalid input. Please try again.");
             }
         }
